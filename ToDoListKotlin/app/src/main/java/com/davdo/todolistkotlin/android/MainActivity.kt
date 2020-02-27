faf4e5e775ea8936d8d775876d09d9f813bd5af5
David Baker <david.baker542@gmail.com>
2020-02-27 15:24:34 -0500

    Code cleanup

--- ToDoListKotlin/app/src/main/java/com/davdo/todolistkotlin/android/MainActivity.kt
+++ ToDoListKotlin/app/src/main/java/com/davdo/todolistkotlin/android/MainActivity.kt
@@ -2,6 +2,8 @@ package com.davdo.todolistkotlin.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.davdo.todolistkotlin.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myFragment: Fragment? = supportFragmentManager.findFragmentById(R.id.fragment_note_container)
+
+        if(myFragment == null) {
+            myFragment = NoteFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_note_container, myFragment).commit()
+        }
     }
 }
