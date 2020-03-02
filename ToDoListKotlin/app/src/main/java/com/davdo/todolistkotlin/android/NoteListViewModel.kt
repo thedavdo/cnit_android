7f182235fa78bedd64bc64d4119957e5a4814bc8
David Baker <david.baker542@gmail.com>
2020-03-02 16:00:49 -0500

    Updated ViewModel to use mutableMap

--- ToDoListKotlin/app/src/main/java/com/davdo/todolistkotlin/android/NoteListViewModel.kt
+++ ToDoListKotlin/app/src/main/java/com/davdo/todolistkotlin/android/NoteListViewModel.kt
@@ -1,16 +1,14 @@
 package com.davdo.todolistkotlin.android
 
+import android.content.Intent
 import androidx.lifecycle.ViewModel
 import com.davdo.todolistkotlin.src.Note
-import java.util.Calendar
-import java.util.Random
+import java.util.*
 import kotlin.math.abs
 
 class NoteListViewModel : ViewModel() {
 
-
-    val notes = mutableListOf<Note>()
-
+    val notes = mutableMapOf<UUID, Note>() //mutableListOf<Note>()
 
     fun generateExamples() {
 
@@ -22,9 +20,8 @@ class NoteListViewModel : ViewModel() {
         for (i in countNotes until countNotes + 20) {
             cal.timeInMillis = abs(r.nextLong())
             val tempNote = Note("My Note #$i", cal.time, r.nextBoolean())
-            notes += tempNote
+//            notes += tempNote
+            notes[tempNote.uuid] = tempNote
         }
     }
-
-
 }
